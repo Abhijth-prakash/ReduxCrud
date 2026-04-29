c04eca81f246d6e4545b1072a192ffae617a583d
abhijith <abhijithabs125@gmail.com>
2026-04-29 19:44:49 +0530

    fixed bugs

--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -6,41 +6,38 @@ import { useNavigate, useParams } from 'react-router-dom'
 
 const EditUser = () => {
     const {id} = useParams()
-    useEffect(() => {
-     dispatch(getUsers())
-}, [])
-
-    const {users,loading} = useSelector(state=>state.users)
-    const user = users.find(items=>items.id == id)
-    const dispatch= useDispatch()
-
-
-const {register, handleSubmit,reset} = useForm({
-    defaultValues: {
-        name: user?.name,   
-        email: user?.email,
-        age: user?.age
-    }
-})
+    const dispatch = useDispatch()
+    const {users, loading} = useSelector(state => state.users)
+    const user = users.find(items => items.id == id)
     const navigate = useNavigate()
+    const {register, handleSubmit, reset} = useForm()
+
+    useEffect(() => {
+        dispatch(getUsers())
+    }, [])
+
+    useEffect(() => {
+        if(user) reset({ name: user.name, email: user.email, age: user.age })
+    }, [user])
+
     if(!user) return <p>user not found</p>
     if(loading) return <p>loading...</p>
-    
 
-    const editFunction =(data)=>{
-        dispatch(updateUser({id, userData: data})) 
+    const editFunction = (data) => {
+        dispatch(updateUser({id, userData: data}))
         navigate('/')
     }
-  return (
-    <div>
-      <form onSubmit={handleSubmit(editFunction)} >
-        <input type="text" {...register("name")} />
-        <input type="email" {...register("email")} />
-        <input type="age" {...register("age")} />
-        <button>submit</button>
-      </form>
-    </div>
-  )
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(editFunction)}>
+                <input type="text" {...register("name")} />
+                <input type="email" {...register("email")} />
+                <input type="text" {...register("age")} />
+                <button>submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default EditUser
