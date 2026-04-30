b7ea779a420a6f16f359b82872551a5b512f3c2f
abhijith <abhijithabs125@gmail.com>
2026-04-30 11:49:45 +0530

    Added zod for validation

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -2,25 +2,43 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
 import { addUser } from '../redux/slices/userSlice'
-const AddUser = () => {
-    const {register,reset,handleSubmit,formState} = useForm()
-    const dispatch = useDispatch()
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-    const addingLogic = (data)=>{
-            dispatch(addUser(data))
-            reset()
-    }
+const schema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Email is invalid"),
+  age: z.coerce.number().min(1, "Age is required"), 
+});
+
+const AddUser = () => {
+  const { register, reset, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(schema), 
+  });
+
+  const dispatch = useDispatch();
+
+  const addingLogic = (data) => {
+    dispatch(addUser(data));
+    reset();
+  };
+
   return (
     <div>
-        <form onSubmit={handleSubmit(addingLogic)} >
-            <input type="text" {...register("name",{required:true})} placeholder='name' />
-            <input type="email" {...register("email",{required:true})} placeholder='email' />
-            <input type="text" {...register("age",{required:true})}  placeholder='age' />
-            <button>submit</button>
-        </form>
-      
-    </div>
-  )
-}
+      <form onSubmit={handleSubmit(addingLogic)}>
+        <input type="text" {...register("name")} placeholder="name" />
+        <p style={{color:"black"}}>{errors.name?.message}</p>
 
-export default AddUser
+        <input type="email" {...register("email")} placeholder="email" />
+        <p style={{color:"black"}}>{errors.email?.message}</p>
+
+        <input type="text" {...register("age")} placeholder="age" />
+       <p style={{color:"black"}}>{errors.age?.message}</p>
+
+        <button>submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -3,6 +3,8 @@ import { useForm } from 'react-hook-form'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUsers, updateUser } from '../redux/slices/userSlice'
 import { useNavigate, useParams } from 'react-router-dom'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 const EditUser = () => {
     const {id} = useParams()
@@ -10,7 +12,15 @@ const EditUser = () => {
     const {users, loading} = useSelector(state => state.users)
     const user = users.find(items => items.id == id)
     const navigate = useNavigate()
-    const {register, handleSubmit, reset} = useForm()
+    const schema = z.object({
+        name:z.string().min(1,"name is required"),
+        email:z.string().email("email is invalid"),
+        age: z.coerce.number().min(1, "Age is required") 
+    })
+
+    const {register, handleSubmit, reset,formState: { errors }} = useForm({
+        resolver:zodResolver(schema)
+    })
 
     useEffect(() => {
         dispatch(getUsers())
@@ -20,20 +30,26 @@ const EditUser = () => {
         if(user) reset({ name: user.name, email: user.email, age: user.age })
     }, [user])
 
-    if(!user) return <p>user not found</p>
     if(loading) return <p>loading...</p>
+    if(!user) return <p>user not found</p>
+    
 
     const editFunction = (data) => {
         dispatch(updateUser({id, userData: data}))
         navigate('/')
     }
 
+
+
     return (
         <div>
             <form onSubmit={handleSubmit(editFunction)}>
                 <input type="text" {...register("name")} />
+                <p style={{color:"black"}}>{errors.name?.message}</p>
                 <input type="email" {...register("email")} />
+                 <p style={{color:"black"}}>{errors.email?.message}</p>
                 <input type="text" {...register("age")} />
+                <p style={{color:"black"}}>{errors.age?.message}</p>
                 <button>submit</button>
             </form>
         </div>
